422ea302b4c48dd41718f126b446223e6cd9fab9
Cherdakov Ravil <cherdakov@itc-media.ru>
2023-04-02 23:13:45 +0500

    Задача 2 из блока 01-basics

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,30 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const vm = createApp({
+    data() {
+      return {
+        one: 0,
+        two: 0,
+        operator: ''
+      };
+    },
+    methods: {
+       summ()  {
+        let total = 0
+        if (this.operator == 'sum')
+            total = this.one + this.two
+        else if(this.operator == 'subtract')
+            total = this.one - this.two
+        else if(this.operator == 'multiply')
+            total = this.one * this.two
+        else if(this.operator == 'divide')
+            total = this.one / this.two
+        return total
+       }
+    },
+    computed: {
+        total() {
+            return this.summ()
+        }
+    }
+    }).mount('#app');
