f1ab89ceaec6aa36fd6393146ca0f5eea853ac6f
Cherdakov Ravil <cherdakov@itc-media.ru>
2023-04-03 08:18:38 +0500

    пулл

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
@@ -30,3 +30,19 @@ const emails = [
 ];
 
 // Требуется создать Vue приложение
+const vm = createApp({
+  data() {
+    return {
+      email: emails,
+      selected: ''
+    };
+  },
+  methods: {
+    selectedEmail() {
+
+    }
+  },
+  computed: {
+    
+  }
+  }).mount('#app');
--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,37 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
+
+const vm = createApp({
+  data() {
+    return {
+      meetup: '',
+    };
+  },
+  methods: {
+    fetchMeetupById(meetupId) {
+      // console.log(meetupId)
+      return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
       });
     }
-  });
-}
-
-// Требуется создать Vue приложение
+  },
+  watch: {
+    meetupSelect() {
+      console.log(id)
+      console.log(meetup)
+      //return this.fetchMeetupById(this.meetupID)
+    }
+    
+  },
+  computed: {
+    
+  }
+  }).mount('#app');
