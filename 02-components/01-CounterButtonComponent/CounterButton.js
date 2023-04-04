250a4d4b3940b92dac2e60755a3e002cba2bfd39
Чердаков Равиль Рашидович <cherdakovrr@uriit.ru>
2023-04-04 18:13:36 +0500

    ver 1

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -2,8 +2,27 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'CounterButton',
-
+  props: {
+    count:{
+        type: Number,
+        default: () => 0,
+      },
+      setCount: {
+        type: Function,
+        require: true
+    },
+  },
+  methods: {
+    
+  },
+  computed: {
+    send(value) {
+      console.log(value)
+      this.setCount(value + 1)
+      //return value + 1
+    }
+  },
   // Компонент должен иметь входной параметр и порождать событие
 
-  template: `<button type="button">1</button>`,
+  template: `<button type="button" @click="send(count)">{{count}}</button>`,
 });
