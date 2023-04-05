833f8d35258ac40d03306e414028d2593124f524
Cherdakov Ravil <cherdakov@itc-media.ru>
2023-04-06 04:31:40 +0500

    Fix Дата 02-components/01 CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -7,24 +7,7 @@ export default defineComponent({
         type: Number,
         default: () => 0,
       },
-      // setCount: {
-      //   type: Function,
-      //   require: true
-      // },
   },
-  methods: {
-    // send(value){
-    //   this.count = value + 1
-    //   return this.setCount()
-    // }
-  },
-  computed: {
-    // setCount() {
-    //   //console.log(this.count)
-    //   return this.count
-    // }
-  },
-  // Компонент должен иметь входной параметр и порождать событие
-
+  emits: ['update:count'],
   template: `<button type="button" @click="$emit('update:count',count + 1)">{{count}}</button>`,
 });
