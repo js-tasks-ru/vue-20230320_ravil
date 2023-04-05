59bfa98c1f485391d61c73395653d06f902b49c4
Cherdakov Ravil <cherdakov@itc-media.ru>
2023-04-06 01:49:39 +0500

    + задача 02-components/04-MeetupInfo

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -1,21 +1,35 @@
+import { isInteger } from 'lodash';
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupInfo',
-
+  props: {
+    organizer: {
+      type: String,
+      require: true,
+    },
+    place: {
+      type: String,
+      require: true,
+    },
+    date: {
+      type: isInteger,
+      require: true,
+    },
+  },
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time datetime="2020-01-01">{{ new Date(date).toLocaleDateString("ru-RU") }} г.</time>
       </li>
     </ul>`,
 });
