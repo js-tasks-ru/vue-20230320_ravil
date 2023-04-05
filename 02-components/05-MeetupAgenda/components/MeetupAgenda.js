61da2289d573ca95dce34297403ea3d9858ab7ad
Cherdakov Ravil <cherdakov@itc-media.ru>
2023-04-06 03:48:32 +0500

    + задача 02-components/05-MeetupAgenda

--- 02-components/05-MeetupAgenda/components/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgenda.js
@@ -1,13 +1,21 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
-
+  components: {
+    MeetupAgendaItem
+  },
+  props: {
+    agenda: {
+      type: Array,
+      require: true
+    }
+  },
   template: `
     <ul class="agenda">
-      <li class="agenda__item">
-        <!-- meetup agenda item -->
+      <li class="agenda__item" v-for="item,key in agenda">
+        <meetup-agenda-item :agenda-item="item" />
       </li>
     </ul>`,
 });
